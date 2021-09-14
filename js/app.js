d527dc07f33051384406799cdaf7c76ba30b7133
k1tam <66139832+k1tam@users.noreply.github.com>
2021-09-14 03:53:22 -0400

    added projects, removed forms

--- js/app.js
+++ js/app.js
@@ -1,43 +1,43 @@
 
-var question = document.getElementById("question");
-var comment = document.getElementById("comment");
-var hiring = document.getElementById("hiring");
+// var question = document.getElementById("question");
+// var comment = document.getElementById("comment");
+// var hiring = document.getElementById("hiring");
 
-var qtextbox = document.getElementById("questiontext");
-var ctextbox = document.getElementById("commenttext");
-var htextbox = document.getElementById("hiretext");
-var ratebox = document.getElementById("rate");
+// var qtextbox = document.getElementById("questiontext");
+// var ctextbox = document.getElementById("commenttext");
+// var htextbox = document.getElementById("hiretext");
+// var ratebox = document.getElementById("rate");
 
-// Display Question Text Section
-question.onclick = function () {
-    qtextbox.style.display = "";
-    ctextbox.style.display = "none";
-    htextbox.style.display = "none";
-    ratebox.style.display = "none";
-}
+// // Display Question Text Section
+// question.onclick = function () {
+//     qtextbox.style.display = "";
+//     ctextbox.style.display = "none";
+//     htextbox.style.display = "none";
+//     ratebox.style.display = "none";
+// }
 
-// Display Comment Text Section
-comment.onclick = function () {
-    ctextbox.style.display = "";
-    qtextbox.style.display = "none";
-    htextbox.style.display = "none";
-    ratebox.style.display = "none";
+// // Display Comment Text Section
+// comment.onclick = function () {
+//     ctextbox.style.display = "";
+//     qtextbox.style.display = "none";
+//     htextbox.style.display = "none";
+//     ratebox.style.display = "none";
 
-}
+// }
 
-// Display Hiring Text Section + Rate of Pay Input Box
-hiring.onclick = function () {
-    htextbox.style.display = "";
-    qtextbox.style.display = "none";
-    ctextbox.style.display = "none";
-    ratebox.style.display = "";
+// // Display Hiring Text Section + Rate of Pay Input Box
+// hiring.onclick = function () {
+//     htextbox.style.display = "";
+//     qtextbox.style.display = "none";
+//     ctextbox.style.display = "none";
+//     ratebox.style.display = "";
 
-}
+// }
 
 // change background colours based on scroll location
 function backgroundChange() 
 {
-    if(this.scrollY > (this.innerHeight * (9.8/10))) // if scroll gets past a certain point
+    if(this.scrollY > (this.innerHeight * (9.9/10))) // if scroll gets past a certain point
     {
         // add the class that has a different background color 
         document.body.classList.add("colorblock");
